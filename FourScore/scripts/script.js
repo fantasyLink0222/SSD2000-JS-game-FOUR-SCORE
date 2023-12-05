4114679f51281e5982e04c43b746e8a62dd74694
Chuan-ssd <fantasylink@gmail.comd>
2023-12-04 19:57:20 -0800

    create

---
+++ FourScore/scripts/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const board = document.getElementById('gameBoard');
+  const rows = 6; // Number of rows
+  const cols = 7; // Number of columns
+  let currentPlayer = 1;
+
+  // Function to create the board
+  const createBoard = () => {
+      for (let i = 0; i < rows * cols; i++) {
+          let cell = document.createElement('div');
+          cell.classList.add('cell');
+          cell.addEventListener('click', () => cellClicked(i));
+          board.appendChild(cell);
+      }
+  };
+
+  // Function to handle cell click
+  const cellClicked = (index) => {
+      // Add logic to handle the cell click
+      // For now, just toggling the player color
+      let cell = board.children[index];
+      cell.classList.add(currentPlayer === 1 ? 'player1' : 'player2');
+      // Toggle player
+      currentPlayer = currentPlayer === 1 ? 2 : 1;
+  };
+
+  createBoard();
+});
